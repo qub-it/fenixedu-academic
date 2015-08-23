15b9060a11acc78d21a9c0fdd1d25f94e468e527
Anil Mamede <anil.mamede@gmail.com>
2015-08-23 11:42:39 +0100

    Annul academic service request debit on cancel or rejection Do not assert payments on cancel or reject academic service request

--- src/main/java/org/fenixedu/academic/domain/serviceRequests/AcademicServiceRequest.java
+++ src/main/java/org/fenixedu/academic/domain/serviceRequests/AcademicServiceRequest.java
@@ -300,12 +300,18 @@ abstract public class AcademicServiceRequest extends AcademicServiceRequest_Base
     final public void reject(final String justification) {
         edit(new AcademicServiceRequestBean(AcademicServiceRequestSituationType.REJECTED, AccessControl.getPerson(),
                 justification));
+
+        Signal.emit(ITreasuryBridgeAPI.ACADEMIC_SERVICE_REQUEST_REJECT_OR_CANCEL_EVENT,
+                new DomainObjectEvent<AcademicServiceRequest>(this));
     }
 
     @Atomic
     final public void cancel(final String justification) {
         edit(new AcademicServiceRequestBean(AcademicServiceRequestSituationType.CANCELLED, AccessControl.getPerson(),
                 justification));
+
+        Signal.emit(ITreasuryBridgeAPI.ACADEMIC_SERVICE_REQUEST_REJECT_OR_CANCEL_EVENT,
+                new DomainObjectEvent<AcademicServiceRequest>(this));
     }
 
     final public void concludeServiceRequest() {
@@ -626,7 +632,10 @@ abstract public class AcademicServiceRequest extends AcademicServiceRequest_Base
         verifyIsToProcessAndHasPersonalInfo(academicServiceRequestBean);
 
         verifyIsToDeliveredAndIsPayed(academicServiceRequestBean);
-        assertPayedEvents();
+        
+        if(!academicServiceRequestBean.isToCancelOrReject()) {
+            assertPayedEvents();
+        }
     }
 
     protected void assertPayedEvents() {
