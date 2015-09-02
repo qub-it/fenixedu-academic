6f6d1d18d14ad90599289feb42a0f3ed4ef020f6
Luís Egídio <luis.egidio@gmail.com>
2015-09-02 18:41:14 +0100

    * Major Change: upon existence of certain curricular rules associated to a given group, prevent automatic enrolment of that group * Major Change, Student enrolment: disable enrolment option for groups with certain curricular rules

--- src/main/java/org/fenixedu/academic/ui/renderers/student/enrollment/bolonha/BolonhaStudentEnrolmentLayout.java
+++ src/main/java/org/fenixedu/academic/ui/renderers/student/enrollment/bolonha/BolonhaStudentEnrolmentLayout.java
@@ -237,7 +237,7 @@ public class BolonhaStudentEnrolmentLayout extends Layout {
         checkBox.setUserValue(enrolmentMetaObject.getKey().toString());
         checkBoxCell.setBody(checkBox);
 
-        if (studentCurriculumGroupBean.isToBeDisabled()) {
+        if (isToDisableEnrolmentOption(studentCurriculumGroupBean)) {
             checkBox.setDisabled(true);
         } else {
             enrollmentsController.addCheckBox(checkBox);
@@ -377,6 +377,7 @@ public class BolonhaStudentEnrolmentLayout extends Layout {
                 HtmlCheckBox checkBox = new HtmlCheckBox(false);
                 checkBox.setName("degreeModuleToEnrolCheckBox" + degreeModuleToEvaluate.getKey());
                 checkBox.setUserValue(degreeModuleToEvaluate.getKey());
+                checkBox.setVisible(!isToDisableEnrolmentOption(degreeModuleToEvaluate));
                 getDegreeModulesToEvaluateController().addCheckBox(checkBox);
                 checkBoxCell.setBody(checkBox);
             } else {
@@ -390,8 +391,13 @@ public class BolonhaStudentEnrolmentLayout extends Layout {
                 final HtmlActionLink actionLink = new HtmlActionLink();
                 actionLink.setText(BundleUtil.getString(Bundle.STUDENT, "label.chooseOptionalCurricularCourse"));
                 actionLink.setController(new OptionalCurricularCourseLinkController(degreeModuleToEvaluate));
-                actionLink
-                        .setOnClick("$(this).closest('form').find('input[name=\\'method\\']').attr('value', 'prepareChooseOptionalCurricularCourseToEnrol');");
+                if (isToDisableEnrolmentOption(degreeModuleToEvaluate)) {
+                    actionLink.setOnClick("function(){return false;}");
+                    actionLink.setStyle("text-decoration: line-through; color: grey; border-bottom: none;");
+                } else {
+                    actionLink
+                            .setOnClick("$(this).closest('form').find('input[name=\\'method\\']').attr('value', 'prepareChooseOptionalCurricularCourseToEnrol');");
+                }
                 //actionLink.setOnClick("document.forms[2].method.value='prepareChooseOptionalCurricularCourseToEnrol';");
                 actionLink.setName("optionalCurricularCourseLink" + degreeModuleToEvaluate.getCurriculumGroup().getExternalId()
                         + "_" + degreeModuleToEvaluate.getContext().getExternalId());
@@ -402,6 +408,12 @@ public class BolonhaStudentEnrolmentLayout extends Layout {
                 encodeCurricularRules(groupTable, degreeModuleToEvaluate);
             }
         }
+    }
+
+    private boolean isToDisableEnrolmentOption(final StudentCurriculumGroupBean input) {
+        return input.isToBeDisabled()
+                || (isStudentLogged() && appliesAnyRules(input.getCurriculumModule(),
+                        CurricularRuleType.ENROLMENT_TO_BE_APPROVED_BY_COORDINATOR));
     }
 
     private boolean isToDisableEnrolmentOption(final IDegreeModuleToEvaluate input) {
@@ -421,20 +433,48 @@ public class BolonhaStudentEnrolmentLayout extends Layout {
 
             for (final CurricularRuleType curricularRuleType : curricularRuleTypes) {
 
-                final CourseGroup courseGroup = input.getCurriculumGroup().getDegreeModule();
+                final CourseGroup parentCourseGroup = input.getCurriculumGroup().getDegreeModule();
 
                 // check self rules
                 final ExecutionSemester executionInterval = this.bolonhaStudentEnrollmentBean.getExecutionPeriod();
                 List<? extends ICurricularRule> rules =
-                        input.getDegreeModule().getCurricularRules(curricularRuleType, courseGroup, executionInterval);
+                        input.getDegreeModule().getCurricularRules(curricularRuleType, parentCourseGroup, executionInterval);
 
                 if (!rules.isEmpty() && rules.iterator().next().appliesToContext(input.getContext())) {
                     return true;
                 }
 
                 // check parent group rules
-                rules = courseGroup.getCurricularRules(curricularRuleType, executionInterval);
+                rules = parentCourseGroup.getCurricularRules(curricularRuleType, executionInterval);
                 if (!rules.isEmpty() && rules.iterator().next().appliesToContext(input.getContext())) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean appliesAnyRules(final CurriculumGroup input, final CurricularRuleType... curricularRuleTypes) {
+
+        if (input != null && curricularRuleTypes != null) {
+
+            for (final CurricularRuleType curricularRuleType : curricularRuleTypes) {
+
+                final CurriculumGroup parentCurriculumGroup = input.getCurriculumGroup();
+                final CourseGroup parentCourseGroup = parentCurriculumGroup.getDegreeModule();
+
+                // check self rules
+                final ExecutionSemester executionInterval = this.bolonhaStudentEnrollmentBean.getExecutionPeriod();
+                List<? extends ICurricularRule> rules =
+                        input.getDegreeModule().getCurricularRules(curricularRuleType, parentCourseGroup, executionInterval);
+
+                if (!rules.isEmpty()) {
+                    return true;
+                }
+
+                // check parent group rules, recursively until root
+                if (!parentCurriculumGroup.isRoot() && appliesAnyRules(parentCurriculumGroup, curricularRuleTypes)) {
                     return true;
                 }
             }
