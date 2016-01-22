1b5520bb1fb3362de8aa1cd7006512d2db32524b
Luís Egídio <luis.egidio@gmail.com>
2016-01-22 17:02:41 +0000

    [JIRA] (ACDM-980) Bug Fix, Enrolments in Special Season: allow to choose EvaluationSeason

--- src/main/java/org/fenixedu/academic/domain/StudentCurricularPlan.java
+++ src/main/java/org/fenixedu/academic/domain/StudentCurricularPlan.java
@@ -29,6 +29,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -1643,8 +1644,11 @@ public class StudentCurricularPlan extends StudentCurricularPlan_Base {
             for (final EnrolmentEvaluation evaluation : enrolment.getEvaluationsSet()) {
                 final EvaluationSeason season = evaluation.getEvaluationSeason();
 
-                if (season.isSpecial() && enrolment.isEnroledInSeason(season, input)) {
-                    result.add(evaluation);
+                if (season.isSpecial()) {
+                    final Optional<EnrolmentEvaluation> search = enrolment.getEnrolmentEvaluation(season, input, (Boolean) null);
+                    if (search.isPresent() && search.get() == evaluation) {
+                        result.add(evaluation);
+                    }
                 }
             }
         }
@@ -1701,8 +1705,11 @@ public class StudentCurricularPlan extends StudentCurricularPlan_Base {
             for (final EnrolmentEvaluation evaluation : enrolment.getEvaluationsSet()) {
                 final EvaluationSeason season = evaluation.getEvaluationSeason();
 
-                if (season.isImprovement() && enrolment.isEnroledInSeason(season, input)) {
-                    result.add(evaluation);
+                if (season.isImprovement()) {
+                    final Optional<EnrolmentEvaluation> search = enrolment.getEnrolmentEvaluation(season, input, (Boolean) null);
+                    if (search.isPresent() && search.get() == evaluation) {
+                        result.add(evaluation);
+                    }
                 }
             }
         }
