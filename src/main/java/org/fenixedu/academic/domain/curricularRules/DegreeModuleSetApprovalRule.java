0b59d892aec82eae601244b85fe5aa2603870a52
Nadir Amin <nadir.amin@qub-it.com>
2024-03-07 11:39:29 +0000

    Improvement: Add option to test only enroled modules on degree modulet set conclusion rule
    issue: #qubIT-Fenix-5030

--- src/main/java/org/fenixedu/academic/domain/curricularRules/DegreeModuleSetApprovalRule.java
+++ src/main/java/org/fenixedu/academic/domain/curricularRules/DegreeModuleSetApprovalRule.java
@@ -3,6 +3,7 @@ package org.fenixedu.academic.domain.curricularRules;
 import static org.fenixedu.academic.util.Bundle.APPLICATION;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -44,7 +45,7 @@ public class DegreeModuleSetApprovalRule extends DegreeModuleSetApprovalRule_Bas
             final ExecutionYear executionYear = enrolmentContext.getExecutionYear();
             final CourseGroup groupForApprovals = rule.getCourseGroupForApprovals();
 
-            final Set<DegreeModule> modulesMissing = rule.getDegreeModulesToApproveSet().stream()
+            final Set<DegreeModule> modulesMissing = rule.getDegreeModulesToApproveFor(curricularPlan).stream()
                     .filter(dm -> !isConcluded(curricularPlan, dm, groupForApprovals, executionYear))
                     .filter(dm -> !canConclude(curricularPlan, dm, groupForApprovals, executionYear))
                     .filter(dm -> !isEnrolling(enrolmentContext, dm)).collect(Collectors.toSet());
@@ -55,7 +56,7 @@ public class DegreeModuleSetApprovalRule extends DegreeModuleSetApprovalRule_Bas
 
             final DegreeModule moduleToApplyRule = rule.getDegreeModuleToApplyRule();
 
-            if (groupForApprovals == null) {
+            if (groupForApprovals == null || moduleToApplyRule == groupForApprovals) {
                 return RuleResult.createWarning(moduleToEvaluate, "label.DegreeModuleSetApprovalRule.conclusion.warning",
                         moduleToApplyRule.getName(), buildModulesToApproveText(modulesMissing));
             }
@@ -71,18 +72,19 @@ public class DegreeModuleSetApprovalRule extends DegreeModuleSetApprovalRule_Bas
     }
 
     public DegreeModuleSetApprovalRule(DegreeModule degreeModule, CourseGroup courseGroup, ExecutionInterval begin,
-            ExecutionInterval end, boolean showWarningOnEnrolment, CourseGroup approvalsGroup,
+            ExecutionInterval end, boolean showWarningOnEnrolment, boolean validateEnroledModulesOnly, CourseGroup approvalsGroup,
             Collection<DegreeModule> degreeModulesToApprove) {
         this();
         super.init(degreeModule, courseGroup, begin, end);
-        edit(courseGroup, begin, end, showWarningOnEnrolment, approvalsGroup, degreeModulesToApprove);
+        edit(courseGroup, begin, end, showWarningOnEnrolment, validateEnroledModulesOnly, approvalsGroup, degreeModulesToApprove);
     }
 
     public void edit(CourseGroup courseGroup, ExecutionInterval begin, ExecutionInterval end, boolean showWarningOnEnrolment,
-            CourseGroup approvalsGroup, Collection<DegreeModule> degreeModulesToApprove) {
+            boolean validateEnroledModulesOnly, CourseGroup approvalsGroup, Collection<DegreeModule> degreeModulesToApprove) {
         super.edit(begin, end);
         super.setContextCourseGroup(courseGroup);
         super.setShowWarningOnEnrolment(showWarningOnEnrolment);
+        super.setValidateEnroledModulesOnly(validateEnroledModulesOnly);
         super.setCourseGroupForApprovals(approvalsGroup);
         super.getDegreeModulesToApproveSet().clear();
         super.getDegreeModulesToApproveSet().addAll(degreeModulesToApprove);
@@ -98,7 +100,7 @@ public class DegreeModuleSetApprovalRule extends DegreeModuleSetApprovalRule_Bas
         if (getCourseGroupForApprovals() != null) {
             final Stream<DegreeModule> parentGroups = getDegreeModulesToApproveSet().stream()
                     .flatMap(dm -> dm.getParentContextsSet().stream().map(ctx -> ctx.getParentCourseGroup()));
-            if (parentGroups.anyMatch(cg -> cg != getCourseGroupForApprovals())) {
+            if (parentGroups.noneMatch(cg -> cg == getCourseGroupForApprovals())) {
                 throw new DomainException(
                         "error.curricularRules.DegreeModuleSetApprovalRule.degreeModulesToApprove.must.belong.to.same.group");
             }
@@ -107,9 +109,9 @@ public class DegreeModuleSetApprovalRule extends DegreeModuleSetApprovalRule_Bas
     }
 
     public List<GenericPair<Object, Boolean>> getLabel() {
-        return List.of(new GenericPair<Object, Boolean>(BundleUtil.getString(APPLICATION,
-                "label.DegreeModuleSetApprovalRule.approval.in", buildModulesToApproveText(getDegreeModulesToApproveSet())),
-                false));
+        final String key = "label.DegreeModuleSetApprovalRule.approval.in" + (getValidateEnroledModulesOnly() ? ".enroled" : "");
+        return List.of(new GenericPair<Object, Boolean>(
+                BundleUtil.getString(APPLICATION, key, buildModulesToApproveText(getDegreeModulesToApproveSet())), false));
     }
 
     private static String buildModulesToApproveText(final Collection<DegreeModule> degreeModules) {
@@ -141,15 +143,36 @@ public class DegreeModuleSetApprovalRule extends DegreeModuleSetApprovalRule_Bas
 
     @Override
     public boolean isConcluded(CurriculumGroup group, ExecutionYear executionYear) {
-        return getDegreeModulesToApproveSet().stream()
+        return getDegreeModulesToApproveFor(group.getStudentCurricularPlan()).stream()
                 .allMatch(dm -> isConcluded(group.getStudentCurricularPlan(), dm, getCourseGroupForApprovals(), executionYear));
     }
 
     @Override
     public boolean canConclude(CurriculumGroup group, ExecutionYear executionYear) {
-        return getDegreeModulesToApproveSet().stream()
+        return getDegreeModulesToApproveFor(group.getStudentCurricularPlan()).stream()
                 .filter(dm -> !isConcluded(group.getStudentCurricularPlan(), dm, getCourseGroupForApprovals(), executionYear))
                 .allMatch(dm -> canConclude(group.getStudentCurricularPlan(), dm, getCourseGroupForApprovals(), executionYear));
+    }
+
+    public Collection<DegreeModule> getDegreeModulesToApproveFor(final StudentCurricularPlan curricularPlan) {
+        if (!getValidateEnroledModulesOnly()) {
+            return getDegreeModulesToApproveSet();
+        }
+
+        final Set<DegreeModule> result = new HashSet<>();
+
+        getDegreeModulesToApproveSet().stream().filter(d -> d.isCourseGroup()).map(CourseGroup.class::cast)
+                .filter(cg -> curricularPlan.findCurriculumGroupFor(cg) != null).forEach(cg -> result.add(cg));
+
+        getDegreeModulesToApproveSet().stream().filter(d -> d.isLeaf()).map(CurricularCourse.class::cast).forEach(cc -> {
+            curricularPlan.getEnrolmentStream().filter(e -> e.hasDegreeModule(cc))
+                    .filter(e -> !e.getCurriculumGroup().isNoCourseGroupCurriculumGroup())
+                    .filter(e -> getCourseGroupForApprovals() == null
+                            || e.getCurriculumGroup().getDegreeModule() == getCourseGroupForApprovals())
+                    .forEach(e -> result.add(cc));
+        });
+
+        return result;
     }
 
     private static boolean isConcluded(StudentCurricularPlan curricularPlan, DegreeModule degreeModule, CourseGroup parentGroup,
