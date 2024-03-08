f73f58a1cbabea3acf6e9fbb1ae870748a345662
rita-macedo <94469199+rita-macedo@users.noreply.github.com>
2024-03-08 14:20:31 +0000

    Feature/20240308/grade calculator new version (#34)
    * Feature: Add Conclusion Grade Calculators  [#qubIT-Fenix-4856]

---
+++ src/main/java/org/fenixedu/academic/domain/student/curriculum/calculator/ConclusionGradeCalculator.java
@@ -0,0 +1,22 @@
+package org.fenixedu.academic.domain.student.curriculum.calculator;
+
+import java.util.stream.Stream;
+
+import org.fenixedu.bennu.core.domain.Bennu;
+
+public abstract class ConclusionGradeCalculator extends ConclusionGradeCalculator_Base {
+
+    protected ConclusionGradeCalculator() {
+        super();
+        setRootDomainObject(Bennu.getInstance());
+    }
+
+    public static Stream<ConclusionGradeCalculator> findAll() {
+        return Bennu.getInstance().getConclusionGradeCalculatorSet().stream();
+    }
+
+    public abstract void delete();
+
+    public abstract String getType();
+
+}
