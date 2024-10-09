b51ce1add860d1e482b96375ce0386ed70590580
Pedro Madeira <pedro.madeira@qub-it.com>
2024-10-09 10:19:12 +0100

    Add enrolment action type enumerator to curriculum logs Relates With: #qubIT-Fenix-5876

---
+++ src/main/java/org/fenixedu/academic/domain/log/EnrolmentActionType.java
@@ -0,0 +1,17 @@
+package org.fenixedu.academic.domain.log;
+
+import org.fenixedu.academic.util.Bundle;
+import org.fenixedu.bennu.core.i18n.BundleUtil;
+
+public enum EnrolmentActionType {
+    
+    ENROL, UNENROL; // ANNUL_ENROLMENT, ACTIVATE_ENROLMENT;
+    
+    public String getName() {
+        return name();
+    }
+
+    public String getLocalizedName() {
+        return BundleUtil.getString(Bundle.ENUMERATION, name());
+    }
+}
