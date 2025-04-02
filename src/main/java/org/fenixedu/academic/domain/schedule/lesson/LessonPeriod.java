579e2b338f28c762e39220215ba1282ad04bde57
Shezad Anavarali <shezad.anavarali@qub-it.com>
2025-04-02 19:03:25 +0100

    Refactor: Add lesson period finder methods
    Issue: #qubIT-Fenix-6667

--- src/main/java/org/fenixedu/academic/domain/schedule/lesson/LessonPeriod.java
+++ src/main/java/org/fenixedu/academic/domain/schedule/lesson/LessonPeriod.java
@@ -1,9 +1,17 @@
 package org.fenixedu.academic.domain.schedule.lesson;
 
+import org.fenixedu.academic.domain.ExecutionCourse;
+import org.fenixedu.academic.domain.ExecutionDegree;
 import org.fenixedu.academic.domain.ExecutionInterval;
 import org.fenixedu.academic.domain.OccupationPeriod;
+import org.fenixedu.academic.domain.degreeStructure.Context;
 import org.fenixedu.bennu.core.domain.Bennu;
 
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LessonPeriod extends LessonPeriod_Base {
@@ -22,6 +30,35 @@ public class LessonPeriod extends LessonPeriod_Base {
 
     public static Stream<LessonPeriod> findAll() {
         return Bennu.getInstance().getLessonPeriodsSet().stream();
+    }
+
+    public static Stream<LessonPeriod> findFor(final ExecutionDegree executionDegree, final ExecutionInterval interval) {
+        return executionDegree.getExecutionDegreeLessonPeriodsSet().stream().map(ExecutionDegreeLessonPeriod::getLessonPeriod)
+                .filter(p -> interval == null || interval == p.getExecutionInterval());
+    }
+
+    public static Stream<LessonPeriod> findFor(final ExecutionDegree executionDegree, final ExecutionCourse executionCourse) {
+        final ExecutionInterval interval = executionCourse.getExecutionInterval();
+
+        final boolean isAnual = executionCourse.getAssociatedCurricularCoursesSet().stream()
+                .filter(cc -> cc.getDegreeCurricularPlan() == executionDegree.getDegreeCurricularPlan())
+                .anyMatch(cc -> cc.isAnual(executionDegree.getExecutionYear()));
+
+        final Set<Integer> courseYears = executionCourse.getAssociatedCurricularCoursesSet().stream()
+                .filter(cc -> cc.getDegreeCurricularPlan() == executionDegree.getDegreeCurricularPlan())
+                .flatMap(cc -> cc.getParentContexts(interval).stream()).map(Context::getCurricularYear).distinct()
+                .collect(Collectors.toSet());
+
+        final Predicate<ExecutionDegreeLessonPeriod> yearsPredicate = p -> Optional.ofNullable(p.getCurricularYears())
+                .map(years -> years.hasAll() || !Collections.disjoint(years.getYears(), courseYears)).orElse(true);
+
+        // if course is anual, show all periods of that year
+        final Predicate<LessonPeriod> intervalPredicate = lp -> isAnual ?
+                interval.getExecutionYear() == lp.getExecutionInterval().getExecutionYear() :
+                interval == lp.getExecutionInterval();
+
+        return executionDegree.getExecutionDegreeLessonPeriodsSet().stream().filter(yearsPredicate)
+                .map(ExecutionDegreeLessonPeriod::getLessonPeriod).filter(intervalPredicate);
     }
 
     public void delete() {
