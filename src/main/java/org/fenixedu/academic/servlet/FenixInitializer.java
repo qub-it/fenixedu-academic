54912b1ee65d2e886ccc6efee3aa97458244a989
arturromao-qubit <artur.romao@qub-it.com>
2025-07-23 12:24:49 +0100

    Refactor: Removes new RAIDES entities initializers and data migration (#132)
    Relates with: #qubIT-Fenix-7009

--- src/main/java/org/fenixedu/academic/servlet/FenixInitializer.java
+++ src/main/java/org/fenixedu/academic/servlet/FenixInitializer.java
@@ -38,24 +38,15 @@ import org.fenixedu.academic.domain.ExecutionInterval;
 import org.fenixedu.academic.domain.Installation;
 import org.fenixedu.academic.domain.OccupationPeriod;
 import org.fenixedu.academic.domain.OccupationPeriodReference;
-import org.fenixedu.academic.domain.ProfessionalSituationConditionType;
-import org.fenixedu.academic.domain.SchoolLevelType;
 import org.fenixedu.academic.domain.dml.DynamicFieldDescriptor;
 import org.fenixedu.academic.domain.dml.DynamicFieldTag;
 import org.fenixedu.academic.domain.organizationalStructure.UnitNamePart;
-import org.fenixedu.academic.domain.raides.DegreeClassification;
 import org.fenixedu.academic.domain.schedule.lesson.ExecutionDegreeLessonPeriod;
 import org.fenixedu.academic.domain.schedule.lesson.LessonPeriod;
-import org.fenixedu.academic.domain.student.personaldata.EducationLevelType;
-import org.fenixedu.academic.domain.student.personaldata.ProfessionCategoryType;
-import org.fenixedu.academic.domain.student.personaldata.ProfessionalStatusType;
 import org.fenixedu.academic.domain.time.calendarStructure.AcademicPeriodOrder;
-import org.fenixedu.academic.util.Bundle;
 import org.fenixedu.bennu.core.api.SystemResource;
 import org.fenixedu.bennu.core.domain.Bennu;
-import org.fenixedu.bennu.core.i18n.BundleUtil;
 import org.fenixedu.bennu.core.rest.Healthcheck;
-import org.fenixedu.commons.i18n.LocalizedString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -92,12 +83,6 @@ public class FenixInitializer implements ServletContextListener {
         initializeLessonPeriods();
 
         initializeExecutionDegreeLessonPeriods();
-
-        initializeEducationLevelTypes();
-        initializeProfessionCategoryTypes();
-        initializeProfessionalStatusTypes();
-        migratePidData();
-        migratePrecedentDegreeInformationData();
     }
 
     @Atomic(mode = TxMode.WRITE)
@@ -215,161 +200,6 @@ public class FenixInitializer implements ServletContextListener {
                 .map(opr -> opr.createCorrespondingExecutionDegreeLessonPeriodIfMissing()).filter(Objects::nonNull).toList();
 
         Log.warn("Finished population of Execution Degree Lesson Periods. Created: " + newPeriods.size());
-        Log.warn("---------------------------------------");
-    }
-
-    @Atomic(mode = TxMode.WRITE)
-    private void initializeEducationLevelTypes() {
-        if (EducationLevelType.findAll().findAny().isPresent()) {
-            return;
-        }
-
-        Log.warn("---------------------------------------");
-        Log.warn("Starting population of Education Level Types");
-
-        for (SchoolLevelType schoolLevelType : SchoolLevelType.values()) {
-            String code = schoolLevelType.getName();
-            LocalizedString name = BundleUtil.getLocalizedString(Bundle.ENUMERATION, schoolLevelType.getQualifiedName());
-
-            if (EducationLevelType.findByCode(code).isEmpty()) {
-                EducationLevelType educationLevelType = EducationLevelType.create(code, name, true);
-
-                schoolLevelType.getEquivalentDegreeClassifications().forEach(c -> {
-                    DegreeClassification classification = DegreeClassification.readByCode(c);
-
-                    if (classification != null) {
-                        educationLevelType.addDegreeClassifications(classification);
-                    }
-                });
-            }
-        }
-
-        Log.warn("Finished population of Education Level Types. Instances created: " + EducationLevelType.findAll().count());
-        Log.warn("---------------------------------------");
-    }
-
-    @Atomic(mode = TxMode.WRITE)
-    private void initializeProfessionCategoryTypes() {
-        if (ProfessionCategoryType.findAll().findAny().isPresent()) {
-            return;
-        }
-
-        Log.warn("---------------------------------------");
-        Log.warn("Starting population of Profession Category Types");
-
-        for (org.fenixedu.academic.domain.ProfessionType professionType : org.fenixedu.academic.domain.ProfessionType.values()) {
-            String code = professionType.getName();
-            LocalizedString name = BundleUtil.getLocalizedString(Bundle.ENUMERATION, professionType.getQualifiedName());
-
-            if (ProfessionCategoryType.findByCode(code).isEmpty()) {
-                ProfessionCategoryType.create(code, name, professionType.isActive());
-            }
-        }
-
-        Log.warn("Finished population of Profession Category Types. Instances created: " + ProfessionCategoryType.findAll()
-                .count());
-        Log.warn("---------------------------------------");
-    }
-
-    @Atomic(mode = TxMode.WRITE)
-    private void initializeProfessionalStatusTypes() {
-        if (ProfessionalStatusType.findAll().findAny().isPresent()) {
-            return;
-        }
-
-        Log.warn("---------------------------------------");
-        Log.warn("Starting population of Professional Status Types");
-
-        for (ProfessionalSituationConditionType professionalSituation : ProfessionalSituationConditionType.values()) {
-            String code = professionalSituation.getName();
-            LocalizedString name = BundleUtil.getLocalizedString(Bundle.ENUMERATION, professionalSituation.getQualifiedName());
-
-            if (ProfessionalStatusType.findByCode(code).isEmpty()) {
-                ProfessionalStatusType.create(code, name, professionalSituation.isActive());
-            }
-        }
-
-        Log.warn("Finished population of Professional Status Types. Instances created: " + ProfessionalStatusType.findAll()
-                .count());
-        Log.warn("---------------------------------------");
-    }
-
-    @Atomic(mode = TxMode.WRITE)
-    private void migratePidData() {
-        Log.warn("---------------------------------------");
-        Log.warn("Migrating Personal Ingression Data fields to new entities");
-
-        Bennu.getInstance().getPersonalIngressionsDataSet().forEach(pid -> {
-            if (pid.getProfessionType() != null && pid.getProfessionCategoryType() == null) {
-                ProfessionCategoryType.findByCode(pid.getProfessionType().getName()).ifPresent(pid::setProfessionCategoryType);
-            }
-
-            if (pid.getMotherProfessionType() != null && pid.getMotherProfessionCategoryType() == null) {
-                ProfessionCategoryType.findByCode(pid.getMotherProfessionType().getName())
-                        .ifPresent(pid::setMotherProfessionCategoryType);
-            }
-
-            if (pid.getFatherProfessionType() != null && pid.getFatherProfessionCategoryType() == null) {
-                ProfessionCategoryType.findByCode(pid.getFatherProfessionType().getName())
-                        .ifPresent(pid::setFatherProfessionCategoryType);
-            }
-
-            if (pid.getSpouseProfessionType() != null && pid.getSpouseProfessionCategoryType() == null) {
-                ProfessionCategoryType.findByCode(pid.getSpouseProfessionType().getName())
-                        .ifPresent(pid::setSpouseProfessionCategoryType);
-            }
-
-            if (pid.getProfessionalCondition() != null && pid.getProfessionalStatusType() == null) {
-                ProfessionalStatusType.findByCode(pid.getProfessionalCondition().getName())
-                        .ifPresent(pid::setProfessionalStatusType);
-            }
-
-            if (pid.getMotherProfessionalCondition() != null && pid.getMotherProfessionalStatusType() == null) {
-                ProfessionalStatusType.findByCode(pid.getMotherProfessionalCondition().getName())
-                        .ifPresent(pid::setMotherProfessionalStatusType);
-            }
-
-            if (pid.getFatherProfessionalCondition() != null && pid.getFatherProfessionalStatusType() == null) {
-                ProfessionalStatusType.findByCode(pid.getFatherProfessionalCondition().getName())
-                        .ifPresent(pid::setFatherProfessionalStatusType);
-            }
-
-            if (pid.getSpouseProfessionalCondition() != null && pid.getSpouseProfessionalStatusType() == null) {
-                ProfessionalStatusType.findByCode(pid.getSpouseProfessionalCondition().getName())
-                        .ifPresent(pid::setSpouseProfessionalStatusType);
-            }
-
-            if (pid.getMotherSchoolLevel() != null && pid.getMotherEducationLevelType() == null) {
-                EducationLevelType.findByCode(pid.getMotherSchoolLevel().getName()).ifPresent(pid::setMotherEducationLevelType);
-            }
-
-            if (pid.getFatherSchoolLevel() != null && pid.getFatherEducationLevelType() == null) {
-                EducationLevelType.findByCode(pid.getFatherSchoolLevel().getName()).ifPresent(pid::setFatherEducationLevelType);
-            }
-
-            if (pid.getSpouseSchoolLevel() != null && pid.getSpouseEducationLevelType() == null) {
-                EducationLevelType.findByCode(pid.getSpouseSchoolLevel().getName()).ifPresent(pid::setSpouseEducationLevelType);
-            }
-        });
-
-        Log.warn("Finished migrating Personal Ingression Data fields to new entities. Processed " + Bennu.getInstance()
-                .getPersonalIngressionsDataSet().size() + " Personal Ingression Data instances.");
-        Log.warn("---------------------------------------");
-    }
-
-    @Atomic(mode = TxMode.WRITE)
-    private void migratePrecedentDegreeInformationData() {
-        Log.warn("---------------------------------------");
-        Log.warn("Migrating Precedent Degree Information fields to new entities");
-
-        Bennu.getInstance().getPrecedentDegreeInformationSet().forEach(pdi -> {
-            if (pdi.getSchoolLevel() != null && pdi.getEducationLevelType() == null) {
-                EducationLevelType.findByCode(pdi.getSchoolLevel().getName()).ifPresent(pdi::setEducationLevelType);
-            }
-        });
-
-        Log.warn("Finished migrating Precedent Degree Information fields to new entities. Processed " + Bennu.getInstance()
-                .getPrecedentDegreeInformationSet().size() + " Precedent Degree Information instances.");
         Log.warn("---------------------------------------");
     }
 }
