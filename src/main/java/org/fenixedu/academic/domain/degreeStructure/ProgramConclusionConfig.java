55e4a62e751221a51c2d31471b2911e14536770e
Artur Romão <artur.romao@qub-it.com>
2025-08-08 17:25:26 +0100

    Implements ProgramConclusionConfiguration to allow ProgramConclusions with multiple DegreeModules Relates with: #qubIT-Fenix-6554

---
+++ src/main/java/org/fenixedu/academic/domain/degreeStructure/ProgramConclusionConfig.java
@@ -0,0 +1,126 @@
+package org.fenixedu.academic.domain.degreeStructure;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.fenixedu.academic.domain.DegreeCurricularPlan;
+import org.fenixedu.academic.domain.exceptions.DomainException;
+import org.fenixedu.bennu.core.domain.Bennu;
+import org.fenixedu.commons.i18n.LocalizedString;
+
+public class ProgramConclusionConfig extends ProgramConclusionConfig_Base {
+    
+    public ProgramConclusionConfig() {
+        super();
+        super.setRootDomainObject(Bennu.getInstance());
+    }
+
+    protected void init(LocalizedString conclusionTitle, DegreeCurricularPlan degreeCurricularPlan,
+            ProgramConclusion programConclusion) {
+        setConclusionTitle(conclusionTitle);
+        setDegreeCurricularPlan(degreeCurricularPlan);
+        setProgramConclusion(programConclusion);
+        changeConfigOrder(degreeCurricularPlan.getProgramConclusionConfigsSet().size() - 1);
+    }
+
+    public static ProgramConclusionConfig create(LocalizedString conclusionTitle, DegreeCurricularPlan degreeCurricularPlan,
+            ProgramConclusion programConclusion) {
+        ProgramConclusionConfig config = new ProgramConclusionConfig();
+        config.init(conclusionTitle, degreeCurricularPlan, programConclusion);
+
+        return config;
+    }
+
+    public void edit(LocalizedString conclusionTitle, ProgramConclusion programConclusion) {
+        setConclusionTitle(conclusionTitle);
+        setProgramConclusion(programConclusion);
+    }
+
+    @Override
+    public void setConfigOrder(final Integer configOrder) {
+        throw new RuntimeException("Order change should be done using move methods");
+    }
+
+    @Override
+    public void addIncludedModules(final DegreeModule includedModule) {
+        if (getExcludedModulesSet().contains(includedModule)) {
+            throw new DomainException("error.ProgramConclusionConfig.degreeModule.is.excluded",
+                    includedModule.getNameI18N().getContent());
+        }
+        super.addIncludedModules(includedModule);
+    }
+
+    @Override
+    public void addExcludedModules(final DegreeModule excludedModule) {
+        if (getIncludedModulesSet().contains(excludedModule)) {
+            throw new DomainException("error.ProgramConclusionConfig.degreeModule.is.included",
+                    excludedModule.getNameI18N().getContent());
+        }
+        super.addExcludedModules(excludedModule);
+    }
+
+    protected void changeConfigOrder(Integer order) {
+        super.setConfigOrder(order);
+    }
+
+    public void moveUp() {
+        final int currentIndex = getConfigOrder();
+        if (currentIndex == 0) {
+            return;
+        }
+
+        final ProgramConclusionConfig toChange = findAtPosition(getDegreeCurricularPlan(), currentIndex - 1);
+        toChange.changeConfigOrder(currentIndex);
+        changeConfigOrder(currentIndex - 1);
+    }
+
+    public void moveTop() {
+        while (getConfigOrder() != 0) {
+            moveUp();
+        }
+    }
+
+    public void moveDown() {
+        final int currentIndex = getConfigOrder();
+        if (currentIndex == find(getDegreeCurricularPlan()).size() - 1) {
+            return;
+        }
+
+        final ProgramConclusionConfig toChange = findAtPosition(getDegreeCurricularPlan(), currentIndex + 1);
+        toChange.changeConfigOrder(currentIndex);
+        changeConfigOrder(currentIndex + 1);
+    }
+
+    public void moveBottom() {
+        while (getConfigOrder() < find(getDegreeCurricularPlan()).size() - 1) {
+            moveDown();
+        }
+    }
+
+    public void delete() {
+        moveBottom();
+
+        if (!getConclusionProcessesSet().isEmpty()) {
+            throw new DomainException("error.ProgramConclusionConfig.cannot.delete.with.related.conclusionProcesses");
+        }
+
+        setProgramConclusion(null);
+        setDegreeCurricularPlan(null);
+        getIncludedModulesSet().clear();
+        getExcludedModulesSet().clear();
+        setRootDomainObject(null);
+        super.deleteDomainObject();
+    }
+
+    public static ProgramConclusionConfig findAtPosition(final DegreeCurricularPlan degreeCurricularPlan, final Integer order) {
+        return find(degreeCurricularPlan).stream().filter(c -> c.getConfigOrder().equals(order)).findFirst().orElse(null);
+    }
+
+    public static Collection<ProgramConclusionConfig> find(final DegreeCurricularPlan degreeCurricularPlan) {
+        if (degreeCurricularPlan == null) {
+            return Collections.emptySet();
+        }
+
+        return degreeCurricularPlan.getProgramConclusionConfigsSet();
+    }
+}
