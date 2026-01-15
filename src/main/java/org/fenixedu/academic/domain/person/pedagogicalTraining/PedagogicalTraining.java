1dbd1af3fcbdd2a92e630fb7383bb55a5eb210e7
Rita Macedo <rita.macedo@qub-it.com>
2026-01-15 14:01:13 +0000

    PR corrections

--- src/main/java/org/fenixedu/academic/domain/person/pedagogicalTraining/PedagogicalTraining.java
+++ src/main/java/org/fenixedu/academic/domain/person/pedagogicalTraining/PedagogicalTraining.java
@@ -1,8 +1,7 @@
 package org.fenixedu.academic.domain.person.pedagogicalTraining;
 
-import java.util.stream.Stream;
-
 import org.fenixedu.academic.domain.Person;
+import org.fenixedu.academic.domain.exceptions.DomainException;
 import org.fenixedu.bennu.core.domain.Bennu;
 import org.fenixedu.commons.i18n.LocalizedString;
 
@@ -13,19 +12,17 @@ public class PedagogicalTraining extends PedagogicalTraining_Base {
         setRoot(Bennu.getInstance());
     }
 
-    protected PedagogicalTraining (LocalizedString description, Person person) {
+
+    public static PedagogicalTraining create (LocalizedString description, Person person) {
+        if (description == null || description.isEmpty()) {
+            throw new DomainException("pedagogicalTraining.description.cannot.be.null");
+        }
         final PedagogicalTraining pedagogicalTraining = new PedagogicalTraining();
 
         pedagogicalTraining.setPerson(person);
         pedagogicalTraining.setDescription(description);
-    }
 
-    public static PedagogicalTraining create (LocalizedString description, Person person) {
-        return new PedagogicalTraining(description, person);
-    }
-
-    public static Stream<PedagogicalTraining> findAllForPerson (Person person) {
-        return person.getPedagogicalTrainingsSet().stream();
+        return pedagogicalTraining;
     }
 
     public void delete() {
