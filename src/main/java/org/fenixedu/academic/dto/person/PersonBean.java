0f345e3c62b37b5dca2807baeb481e9c73daa971
Eduardo Fernandes <eduardo.fernandes@qub-it.com>
2026-03-27 16:19:54 +0000

    Replace PersonBean document id number missing reference Relates with: #qubIT-Fenix-6491

--- src/main/java/org/fenixedu/academic/dto/person/PersonBean.java
+++ src/main/java/org/fenixedu/academic/dto/person/PersonBean.java
@@ -218,7 +218,7 @@ public class PersonBean implements Serializable {
         setDocumentIdEmissionDate(person.getEmissionDateOfDocumentIdYearMonthDay());
         setDocumentIdEmissionLocation(person.getEmissionLocationOfDocumentId());
         setDocumentIdExpirationDate(person.getExpirationDateOfDocumentIdYearMonthDay());
-        setDocumentIdNumber(person.getDocumentIdNumber());
+        setDocumentIdNumber(person.getDefaultIdentificationDocument().getValue());
         setIdentificationDocumentType(person.getDefaultIdentificationDocument().getIdentificationDocumentType());
         setIdentificationDocumentSeriesNumber(person.getIdentificationDocumentSeriesNumber());
         setFiscalCountry(person.getFiscalCountry());
