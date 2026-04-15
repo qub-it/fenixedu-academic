0a3aa1d4d393189e9ac87463a7f085da116bb9c7
Pedro Monteiro <pedro.monteiro@qub-it.com>
2026-04-15 15:04:50 +0100

    Replace references for Person and PersonBean Relates with: #qubIT-Fenix-6491

--- src/main/java/org/fenixedu/academic/domain/Person.java
+++ src/main/java/org/fenixedu/academic/domain/Person.java
@@ -315,9 +315,11 @@ public class Person extends Person_Base {
         // identification
         setIdentification(personBean.getDocumentIdNumber(), personBean.getIdentificationDocumentType());
         setIdentificationDocumentSeriesNumber(personBean.getIdentificationDocumentSeriesNumber());
-        setEmissionLocationOfDocumentId(personBean.getDocumentIdEmissionLocation());
-        setEmissionDateOfDocumentIdYearMonthDay(personBean.getDocumentIdEmissionDate());
-        setExpirationDateOfDocumentIdYearMonthDay(personBean.getDocumentIdExpirationDate());
+
+        getDefaultIdentificationDocument().setEmissionLocation(personBean.getDocumentIdEmissionLocation());
+        getDefaultIdentificationDocument().setEmissionDate(personBean.getDocumentEmissionDate());
+        getDefaultIdentificationDocument().setExpirationDate(personBean.getDocumentExpirationDate());
+
         setEidentifier(personBean.getEidentifier());
         setHealthCardNumber(personBean.getHealthCardNumber());
 
--- src/main/java/org/fenixedu/academic/dto/person/PersonBean.java
+++ src/main/java/org/fenixedu/academic/dto/person/PersonBean.java
@@ -37,6 +37,7 @@ import org.fenixedu.academic.domain.contacts.WebAddress;
 import org.fenixedu.academic.domain.person.Gender;
 import org.fenixedu.academic.domain.person.MaritalStatus;
 import org.fenixedu.academic.domain.person.identificationDocument.IdentificationDocumentType;
+import org.joda.time.LocalDate;
 import org.joda.time.YearMonthDay;
 
 import com.google.common.base.Strings;
@@ -78,7 +79,11 @@ public class PersonBean implements Serializable {
 
     private YearMonthDay documentIdEmissionDate;
 
+    private LocalDate documentEmissionDate;
+
     private YearMonthDay documentIdExpirationDate;
+
+    private LocalDate documentExpirationDate;
 
     private Country fiscalCountry;
 
@@ -217,9 +222,9 @@ public class PersonBean implements Serializable {
         setDistrictOfBirth(person.getDistrictOfBirth());
         setDistrictSubdivisionOfBirth(person.getDistrictSubdivisionOfBirth());
 
-        setDocumentIdEmissionDate(person.getEmissionDateOfDocumentIdYearMonthDay());
-        setDocumentIdEmissionLocation(person.getEmissionLocationOfDocumentId());
-        setDocumentIdExpirationDate(person.getExpirationDateOfDocumentIdYearMonthDay());
+        setDocumentEmissionDate(person.getDefaultIdentificationDocument().getEmissionDate());
+        setDocumentIdEmissionLocation(person.getDefaultIdentificationDocument().getEmissionLocation());
+        setDocumentExpirationDate(person.getDefaultIdentificationDocument().getExpirationDate());
         setDocumentIdNumber(person.getDefaultIdentificationDocument().getValue());
         setIdentificationDocumentType(person.getDefaultIdentificationDocument().getIdentificationDocumentType());
         setIdentificationDocumentSeriesNumber(person.getIdentificationDocumentSeriesNumber());
@@ -409,6 +414,14 @@ public class PersonBean implements Serializable {
         this.documentIdEmissionDate = documentIdEmissionDate;
     }
 
+    public LocalDate getDocumentEmissionDate() {
+        return documentEmissionDate;
+    }
+
+    public void setDocumentEmissionDate(final LocalDate documentEmissionDate) {
+        this.documentEmissionDate = documentEmissionDate;
+    }
+
     public String getDocumentIdEmissionLocation() {
         return documentIdEmissionLocation;
     }
@@ -423,6 +436,14 @@ public class PersonBean implements Serializable {
 
     public void setDocumentIdExpirationDate(final YearMonthDay documentIdExpirationDate) {
         this.documentIdExpirationDate = documentIdExpirationDate;
+    }
+
+    public LocalDate getDocumentExpirationDate() {
+        return documentExpirationDate;
+    }
+
+    public void setDocumentExpirationDate(final LocalDate documentExpirationDate) {
+        this.documentExpirationDate = documentExpirationDate;
     }
 
     public String getDocumentIdNumber() {
@@ -906,9 +927,9 @@ public class PersonBean implements Serializable {
         // identification
         person.setIdentification(this.getDocumentIdNumber(), this.getIdentificationDocumentType());
         person.setIdentificationDocumentSeriesNumber(getIdentificationDocumentSeriesNumber());
-        person.setEmissionLocationOfDocumentId(this.getDocumentIdEmissionLocation());
-        person.setEmissionDateOfDocumentIdYearMonthDay(this.getDocumentIdEmissionDate());
-        person.setExpirationDateOfDocumentIdYearMonthDay(this.getDocumentIdExpirationDate());
+        person.getDefaultIdentificationDocument().setEmissionLocation(this.getDocumentIdEmissionLocation());
+        person.getDefaultIdentificationDocument().setEmissionDate(this.getDocumentEmissionDate());
+        person.getDefaultIdentificationDocument().setExpirationDate(this.getDocumentExpirationDate());
         person.setEidentifier(this.getEidentifier());
         person.setHealthCardNumber(this.healthCardNumber);
 
