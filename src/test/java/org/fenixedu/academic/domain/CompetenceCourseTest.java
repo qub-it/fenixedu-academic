c0c1b50167fe9465cde2378108184a769119a564
Artur Romão <artur.romao@qub-it.com>
2026-07-13 12:31:58 +0100

    Improves CompetenceCourse domain methods and unit tests Relates with: #qubIT-Fenix-8962

--- src/test/java/org/fenixedu/academic/domain/CompetenceCourseTest.java
+++ src/test/java/org/fenixedu/academic/domain/CompetenceCourseTest.java
@@ -1,48 +1,60 @@
 package org.fenixedu.academic.domain;
 
+import static org.fenixedu.academic.domain.degreeStructure.CompetenceCourseTypeTest.initCompetenceCourseType;
+import static org.fenixedu.academic.domain.time.calendarStructure.AcademicPeriod.SEMESTER;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.math.BigDecimal;
+import java.util.Locale;
+import java.util.function.Function;
+
+import org.fenixedu.academic.domain.curricularPeriod.CurricularPeriod;
 import org.fenixedu.academic.domain.curriculum.grade.GradeScale;
+import org.fenixedu.academic.domain.degree.DegreeType;
 import org.fenixedu.academic.domain.degreeStructure.CompetenceCourseInformation;
 import org.fenixedu.academic.domain.degreeStructure.CompetenceCourseLevelType;
 import org.fenixedu.academic.domain.degreeStructure.CompetenceCourseType;
 import org.fenixedu.academic.domain.degreeStructure.CourseLoadType;
+import org.fenixedu.academic.domain.degreeStructure.CurricularStage;
 import org.fenixedu.academic.domain.exceptions.DomainException;
 import org.fenixedu.academic.domain.organizationalStructure.Unit;
 import org.fenixedu.academic.domain.time.calendarStructure.AcademicPeriod;
 import org.fenixedu.academic.util.Bundle;
 import org.fenixedu.bennu.core.i18n.BundleUtil;
 import org.fenixedu.commons.i18n.LocalizedString;
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.FenixFrameworkRunner;
+
 import pt.ist.fenixframework.FenixFramework;
-
-import java.math.BigDecimal;
-import java.util.Locale;
-import java.util.function.Function;
-
-import static org.fenixedu.academic.domain.degreeStructure.CompetenceCourseTypeTest.initCompetenceCourseType;
-import static org.fenixedu.academic.domain.time.calendarStructure.AcademicPeriod.SEMESTER;
-import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @RunWith(FenixFrameworkRunner.class)
 public class CompetenceCourseTest {
 
     public static final String COURSES_UNIT_PATH = "QS>Courses>CC";
-
     public static final String COURSE_A_CODE = "CA";
-
     public static final String COURSE_B_CODE = "CB"; // annual
 
-    private static CompetenceCourse competenceCourseA;
-
-    private static CompetenceCourse competenceCourseB;
+    private static ExecutionYear executionYear, nextExecutionYear;
+    private static ExecutionInterval executionInterval;
+    private static CurricularPeriod firstSemester;
+    private static CompetenceCourse competenceCourseA, competenceCourseB;
+    private static CompetenceCourseInformation courseInformation, nextCourseInformation;
+    private static CurricularCourse testCurricularCourse;
+    private static DegreeCurricularPlan testDegreeCurricularPlan;
+    private static Unit coursesUnit;
 
     @BeforeClass
     public static void init() {
         FenixFramework.getTransactionManager().withTransaction(() -> {
             initCompetenceCourse();
+            initDomainTestData();
             return null;
         });
     }
@@ -55,32 +67,69 @@ public class CompetenceCourseTest {
 
         initCourseLoadTypes();
 
-        Unit coursesUnit = Unit.findInternalUnitByAcronymPath(COURSES_UNIT_PATH).orElseThrow();
-        createCompetenceCourseASemester(coursesUnit);
-        createCompetenceCourseBAnnual(coursesUnit);
+        executionYear = ExecutionYear.findCurrent(null);
+        executionInterval = executionYear.getFirstExecutionPeriod();
+        nextExecutionYear = (ExecutionYear) executionYear.getNext();
+        coursesUnit = Unit.findInternalUnitByAcronymPath(COURSES_UNIT_PATH).orElseThrow();
+
+        createCompetenceCourseASemester();
+        createCompetenceCourseBAnnual();
     }
 
-    private static void createCompetenceCourseASemester(Unit coursesUnit) {
-        competenceCourseA = createCompetenceCourse("Course A", COURSE_A_CODE, new BigDecimal("6.0"), SEMESTER,
-                ExecutionInterval.findFirstCurrentChild(null), coursesUnit);
+    private static void initDomainTestData() {
+        DegreeType degreeType =
+                new DegreeType(new LocalizedString.Builder().with(Locale.getDefault(), "CC Test Degree Type").build());
+        degreeType.setCode("CC_DEGREE_TYPE");
 
-        final CompetenceCourseInformation courseInformation =
+        Degree degree =
+                new Degree("CC Test Degree", "CC Test Degree", "CC_DEGREE", degreeType, new GradeScale(), new GradeScale(),
+                        executionYear);
+        degree.setCode("CC_DEGREE");
+        degree.setCalendar(executionYear.getAcademicInterval().getAcademicCalendar());
+
+        testDegreeCurricularPlan = new DegreeCurricularPlan(degree, "CC_DCP", AcademicPeriod.THREE_YEAR);
+        testDegreeCurricularPlan.setCurricularStage(CurricularStage.APPROVED);
+
+        CurricularPeriod yearPeriod = new CurricularPeriod(AcademicPeriod.YEAR, 1, testDegreeCurricularPlan.getDegreeStructure());
+        firstSemester = new CurricularPeriod(AcademicPeriod.SEMESTER, 1, yearPeriod);
+
+        testCurricularCourse = new CurricularCourse();
+        testCurricularCourse.setCompetenceCourse(competenceCourseA);
+    }
+
+    @After
+    public void cleanup() {
+        CompetenceCourse.findAll().stream().filter(cc -> cc != competenceCourseA && cc != competenceCourseB)
+                .forEach(CompetenceCourse::delete);
+
+        competenceCourseA.getCompetenceCourseInformationsSet().stream()
+                .filter(cci -> cci != courseInformation && cci != nextCourseInformation)
+                .forEach(CompetenceCourseInformation::delete);
+
+        Unit.findInternalUnitByAcronymPath("TCG").ifPresent(Unit::delete);
+        Unit.findInternalUnitByAcronymPath("TD").ifPresent(Unit::delete);
+    }
+
+    private static void createCompetenceCourseASemester() {
+        competenceCourseA = createCompetenceCourse("Course A", COURSE_A_CODE, new BigDecimal("6.0"), SEMESTER, executionInterval,
+                coursesUnit);
+
+        courseInformation =
                 competenceCourseA.getCompetenceCourseInformationsSet().iterator().next();
         courseInformation.setLoadHours(CourseLoadType.of(CourseLoadType.THEORETICAL), new BigDecimal("30.0"));
         courseInformation.setLoadHours(CourseLoadType.of(CourseLoadType.PRACTICAL_LABORATORY), new BigDecimal("10.0"));
         courseInformation.setLoadHours(CourseLoadType.of(CourseLoadType.AUTONOMOUS_WORK), new BigDecimal("20.0"));
 
-        final ExecutionYear nextExecutionYear = (ExecutionYear) ExecutionYear.findCurrentAggregator(null).getNext();
-        final CompetenceCourseInformation nextCourseInformation =
+        nextCourseInformation =
                 new CompetenceCourseInformation(courseInformation, nextExecutionYear.getFirstExecutionPeriod());
 
         nextCourseInformation.setLoadHours(CourseLoadType.of(CourseLoadType.THEORETICAL), new BigDecimal("5.0"));
         nextCourseInformation.setLoadHours(CourseLoadType.of(CourseLoadType.THEORETICAL_PRACTICAL), new BigDecimal("15.0"));
     }
 
-    private static void createCompetenceCourseBAnnual(Unit coursesUnit) {
+    private static void createCompetenceCourseBAnnual() {
         competenceCourseB = createCompetenceCourse("Course B", COURSE_B_CODE, new BigDecimal("15.0"), AcademicPeriod.YEAR,
-                ExecutionInterval.findFirstCurrentChild(null), coursesUnit);
+                executionInterval, coursesUnit);
 
         final CompetenceCourseInformation courseInformation =
                 competenceCourseB.getCompetenceCourseInformationsSet().iterator().next();
@@ -126,13 +175,14 @@ public class CompetenceCourseTest {
     }
 
     @Test
-    public void testCourseLoad_init() {
+    public void testCompetenceCourse_courseLoadType_init() {
         assertNotNull(CourseLoadType.of(CourseLoadType.THEORETICAL));
         assertNotNull(CourseLoadType.of(CourseLoadType.THEORETICAL_PRACTICAL));
         assertNotNull(CourseLoadType.of(CourseLoadType.PRACTICAL_LABORATORY));
         assertEquals(CourseLoadType.of(CourseLoadType.THEORETICAL).getCode(), CourseLoadType.THEORETICAL);
     }
 
+    @Deprecated(forRemoval = true)
     @Test
     public void testCourse_find() {
         assertEquals(CompetenceCourse.find(COURSE_A_CODE), competenceCourseA);
@@ -142,30 +192,30 @@ public class CompetenceCourseTest {
     }
 
     @Test
-    public void testCourse_courseLoad() {
+    public void testCompetenceCourse_courseLoad() {
         assertEquals(competenceCourseA.getTheoreticalHours(), 5d, 0d);
         assertEquals(competenceCourseA.getProblemsHours(), 15d, 0d);
 
-        final ExecutionInterval currentInterval = ExecutionInterval.findFirstCurrentChild(null);
-        assertEquals(competenceCourseA.getTheoreticalHours(currentInterval), 30d, 0d);
-        assertEquals(competenceCourseA.getProblemsHours(currentInterval), 0d, 0d);
+        assertEquals(competenceCourseA.getTheoreticalHours(executionInterval), 30d, 0d);
+        assertEquals(competenceCourseA.getProblemsHours(executionInterval), 0d, 0d);
 
-        final CompetenceCourseInformation informationA = competenceCourseA.findInformationMostRecentUntil(currentInterval);
+        final CompetenceCourseInformation informationA = competenceCourseA.findInformationMostRecentUntil(executionInterval);
 
         assertEquals(informationA.getContactLoad(), new BigDecimal("40.0"));
         assertEquals(informationA.getTotalLoad(), new BigDecimal("60.0"));
 
-        assertEquals(competenceCourseA.getLoadHours(CourseLoadType.of(CourseLoadType.THEORETICAL), currentInterval).orElseThrow(),
+        assertEquals(
+                competenceCourseA.getLoadHours(CourseLoadType.of(CourseLoadType.THEORETICAL), executionInterval).orElseThrow(),
                 new BigDecimal("30.0"));
         assertNotNull(competenceCourseA.getLoadHours(CourseLoadType.of(CourseLoadType.THEORETICAL)).orElse(null));
-        assertNull(competenceCourseA.getLoadHours(CourseLoadType.of(CourseLoadType.THEORETICAL_PRACTICAL), currentInterval)
+        assertNull(competenceCourseA.getLoadHours(CourseLoadType.of(CourseLoadType.THEORETICAL_PRACTICAL), executionInterval)
                 .orElse(null));
         assertEquals(competenceCourseA.getLoadHours(CourseLoadType.of(CourseLoadType.THEORETICAL_PRACTICAL)).orElseThrow(),
                 new BigDecimal("15.0"));
     }
 
     @Test
-    public void testCourse_courseLoadAnnual() {
+    public void testCompetenceCourse_courseLoadAnnual() {
         assertEquals(competenceCourseB.getTheoreticalHours(), 40d, 0d);
         assertEquals(competenceCourseB.getProblemsHours(), 0d, 0d);
         assertEquals(competenceCourseB.getLaboratorialHours(), 5d, 0d);
@@ -178,29 +228,26 @@ public class CompetenceCourseTest {
     }
 
     @Test
-    public void testCourse_credits() {
-        final ExecutionInterval currentInterval = ExecutionInterval.findFirstCurrentChild(null);
-        final CompetenceCourseInformation informationA = competenceCourseA.findInformationMostRecentUntil(currentInterval);
-        final CompetenceCourseInformation informationB = competenceCourseB.findInformationMostRecentUntil(currentInterval);
+    public void testCompetenceCourse_credits() {
+        final CompetenceCourseInformation informationA = competenceCourseA.findInformationMostRecentUntil(executionInterval);
+        final CompetenceCourseInformation informationB = competenceCourseB.findInformationMostRecentUntil(executionInterval);
 
-        assertEquals(competenceCourseA.getEctsCredits(currentInterval), 6d, 0d);
+        assertEquals(competenceCourseA.getEctsCredits(executionInterval), 6d, 0d);
         assertEquals(informationA.getCredits(), new BigDecimal("6.0"));
 
-        assertEquals(competenceCourseB.getEctsCredits(currentInterval), 15d, 0d);
+        assertEquals(competenceCourseB.getEctsCredits(executionInterval), 15d, 0d);
         assertEquals(informationB.getCredits(), new BigDecimal("15.0"));
     }
 
     @Test
-    public void testCourse_creation() {
-        Unit coursesUnit = Unit.findInternalUnitByAcronymPath(COURSES_UNIT_PATH).orElseThrow();
+    public void testCompetenceCourse_creation() {
         CompetenceCourseType competenceCourseType = initCompetenceCourseType();
-        ExecutionInterval startInterval = ExecutionInterval.findFirstCurrentChild(null);
         GradeScale gradeScale = new GradeScale();
         LocalizedString name = new LocalizedString(Locale.getDefault(), "Test Course");
         BigDecimal credits = new BigDecimal("7.5");
 
         CompetenceCourse course = new CompetenceCourse("TC001", name, "TST", credits, coursesUnit, SEMESTER, competenceCourseType,
-                CompetenceCourseLevelType.UNKNOWN().orElse(null), startInterval, gradeScale);
+                CompetenceCourseLevelType.UNKNOWN().orElse(null), executionInterval, gradeScale);
 
         assertEquals("TC001", course.getCode());
         assertEquals(credits.doubleValue(), course.getEctsCredits(), 0d);
@@ -215,25 +262,22 @@ public class CompetenceCourseTest {
         CompetenceCourse courseWitGeneratedAcronym =
                 new CompetenceCourse("GAC001", new LocalizedString(Locale.getDefault(), "Generated Acronym Course"), null,
                         credits, coursesUnit, SEMESTER, competenceCourseType, CompetenceCourseLevelType.UNKNOWN().orElse(null),
-                        startInterval, gradeScale);
+                        executionInterval, gradeScale);
         assertEquals("GAC", courseWitGeneratedAcronym.getAcronym());
     }
 
     @Test
-    public void testCourse_creationWithExistingCode() {
-        Unit coursesUnit = Unit.findInternalUnitByAcronymPath(COURSES_UNIT_PATH).orElseThrow();
-        ExecutionInterval startInterval = ExecutionInterval.findFirstCurrentChild(null);
+    public void testCompetenceCourse_creationWithExistingCode() {
         String existingCode = "EXISTS";
 
-        createCompetenceCourse("A", existingCode, new BigDecimal("1.0"), SEMESTER, startInterval, coursesUnit);
+        createCompetenceCourse("A", existingCode, new BigDecimal("1.0"), SEMESTER, executionInterval, coursesUnit);
 
         assertThrows(DomainException.class,
-                () -> createCompetenceCourse("B", existingCode, new BigDecimal("2.0"), SEMESTER, startInterval, coursesUnit));
+                () -> createCompetenceCourse("B", existingCode, new BigDecimal("2.0"), SEMESTER, executionInterval, coursesUnit));
     }
 
     @Test
-    public void testCourse_creationWithInvalidExecutionIntervals() {
-        Unit coursesUnit = Unit.findInternalUnitByAcronymPath(COURSES_UNIT_PATH).orElseThrow();
+    public void testCompetenceCourse_creationWithInvalidExecutionIntervals() {
         ExecutionInterval startInterval = ExecutionInterval.findCurrentAggregator(null);
 
         assertThrows(DomainException.class,
@@ -245,4 +289,25 @@ public class CompetenceCourseTest {
                         startInterval, coursesUnit));
     }
 
+    @Test
+    public void testCompetenceCourse_delete() {
+        CompetenceCourse competenceCourse =
+                createCompetenceCourse("Course to Delete", "DELETE", new BigDecimal("6.0"), SEMESTER, executionInterval,
+                        coursesUnit);
+
+        assertNotNull(CompetenceCourse.find("DELETE"));
+
+        competenceCourse.delete();
+
+        assertNull(CompetenceCourse.find("DELETE"));
+
+        assertThrows(DomainException.class, () -> competenceCourseA.delete(), "mustDeleteCurricularCoursesFirst");
+    }
+
+    @Test
+    public void testCompetenceCourse_comparatorByName() {
+        assertTrue(CompetenceCourse.COMPETENCE_COURSE_COMPARATOR_BY_NAME.compare(competenceCourseA, competenceCourseB) < 0);
+        assertTrue(CompetenceCourse.COMPETENCE_COURSE_COMPARATOR_BY_NAME.compare(competenceCourseB, competenceCourseA) > 0);
+        assertEquals(0, CompetenceCourse.COMPETENCE_COURSE_COMPARATOR_BY_NAME.compare(competenceCourseA, competenceCourseA));
+    }
 }
