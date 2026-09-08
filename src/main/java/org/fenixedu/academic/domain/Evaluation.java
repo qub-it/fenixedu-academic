22aeb567d13f63bfaed8f51be6adf5ce30e61e65
Pedro Monteiro <pedro.monteiro@qub-it.com>
2026-09-08 12:04:50 +0100

    Refactor: Evaluation domain methods
    Improve Evaluation domain methods
    Relates with: #qubIT-Fenix-9251

--- src/main/java/org/fenixedu/academic/domain/Evaluation.java
+++ src/main/java/org/fenixedu/academic/domain/Evaluation.java
@@ -18,12 +18,8 @@
  */
 package org.fenixedu.academic.domain;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
-import org.fenixedu.academic.domain.student.Registration;
-import org.fenixedu.academic.util.Bundle;
 import org.fenixedu.bennu.core.domain.Bennu;
 
 public abstract class Evaluation extends Evaluation_Base {
@@ -33,42 +29,13 @@ public abstract class Evaluation extends Evaluation_Base {
         setRootDomainObject(Bennu.getInstance());
     }
 
-    public List<ExecutionCourse> getAttendingExecutionCoursesFor(final Registration registration) {
-        final List<ExecutionCourse> result = new ArrayList<ExecutionCourse>();
-        for (final ExecutionCourse executionCourse : this.getAssociatedExecutionCoursesSet()) {
-            if (registration.attends(executionCourse)) {
-                result.add(executionCourse);
-            }
-        }
-        if (result.isEmpty()) { // Then user does not attend any executioncourse
-            result.addAll(this.getAssociatedExecutionCoursesSet());
-        }
-        return result;
-    }
-
     public void delete() {
         this.getAssociatedExecutionCoursesSet().clear();
-        for (; !getMarksSet().isEmpty(); getMarksSet().iterator().next().delete()) {
-            ;
+        while (!getMarksSet().isEmpty()) {
+            getMarksSet().iterator().next().delete();
         }
         setRootDomainObject(null);
         super.deleteDomainObject();
-    }
-
-//    public Mark addNewMark(Attends attends, String markValue) {
-//        if (attends.getMarkByEvaluation(this) != null) {
-//            throw new DomainException("error.Evaluation.attend.already.has.mark.for.evaluation");
-//        }
-//        return new Mark(attends, this, markValue);
-//    }
-
-    public Mark getMarkByAttend(Attends attends) {
-        for (Mark mark : getMarksSet()) {
-            if (mark.getAttend().equals(attends)) {
-                return mark;
-            }
-        }
-        return null;
     }
 
     public boolean isFinal() {
@@ -79,32 +46,8 @@ public abstract class Evaluation extends Evaluation_Base {
         return null;
     }
 
-    protected void logCreate() {
-        logAuxBasic("log.executionCourse.evaluation.generic.created");
-    }
-
-    protected void logEdit() {
-        logAuxBasic("log.executionCourse.evaluation.generic.edited");
-    }
-
-    protected void logRemove() {
-        logAuxBasic("log.executionCourse.evaluation.generic.removed");
-    }
-
-    private void logAuxBasic(String key) {
-        for (ExecutionCourse ec : getAssociatedExecutionCoursesSet()) {
-            EvaluationManagementLog.createLog(ec, Bundle.MESSAGING, key, getPresentationName(), ec.getName(),
-                    ec.getDegreePresentationString());
-        }
-    }
-
     public Date getEvaluationDate() {
         return null;
     }
 
-//    public InfoEvaluation newInfoFromDomain() {
-//        InfoEvaluation infoEvaluation = new InfoEvaluation();
-//        infoEvaluation.copyFromDomain(this);
-//        return infoEvaluation;
-//    }
 }
