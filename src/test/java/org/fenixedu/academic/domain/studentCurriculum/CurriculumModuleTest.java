ba9b3cc5aadf03002e24f2e2f3e417f5b36002ec
Pedro Monteiro <pedro.monteiro@qub-it.com>
2026-09-16 16:26:30 +0100

    Improve CurriculumModule domain methods Relates with: #qubIT-Fenix-9420

---
+++ src/test/java/org/fenixedu/academic/domain/studentCurriculum/CurriculumModuleTest.java
@@ -0,0 +1,123 @@
+package org.fenixedu.academic.domain.studentCurriculum;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
+
+import org.fenixedu.academic.domain.Degree;
+import org.fenixedu.academic.domain.DegreeCurricularPlan;
+import org.fenixedu.academic.domain.DegreeTest;
+import org.fenixedu.academic.domain.ExecutionInterval;
+import org.fenixedu.academic.domain.ExecutionIntervalTest;
+import org.fenixedu.academic.domain.ExecutionYear;
+import org.fenixedu.academic.domain.StudentTest;
+import org.fenixedu.academic.domain.degree.DegreeType;
+import org.fenixedu.academic.domain.degreeStructure.CourseGroup;
+import org.fenixedu.academic.domain.student.Registration;
+import org.fenixedu.academic.domain.student.Student;
+import org.fenixedu.academic.domain.time.calendarStructure.AcademicPeriod;
+import org.fenixedu.academic.domain.util.UserUtil;
+import org.fenixedu.bennu.core.domain.User;
+import org.fenixedu.commons.i18n.LocalizedString;
+import org.joda.time.DateTime;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.FenixFrameworkRunner;
+
+import pt.ist.fenixframework.FenixFramework;
+
+@RunWith(FenixFrameworkRunner.class)
+public class CurriculumModuleTest {
+
+    private static CurriculumGroup groupA;
+    private static CurriculumGroup groupB;
+    private static CurriculumGroup groupC;
+
+    @BeforeClass
+    public static void init() {
+        FenixFramework.getTransactionManager().withTransaction(() -> {
+            ExecutionIntervalTest.initRootCalendarAndExecutionYears();
+            UserUtil.initAdminUser();
+            StudentTest.initRegistrationConfigEntities();
+
+            final ExecutionYear executionYear = ExecutionYear.findCurrent(null);
+            final ExecutionInterval executionInterval = executionYear.getFirstExecutionPeriod();
+
+            final DegreeCurricularPlan dcp = createDcp(executionYear);
+            final Student student = StudentTest.createStudent("Curriculum Module Test Student",
+                    "curriculum.module.test.student." + UUID.randomUUID());
+            final Registration registration = StudentTest.createRegistration(student, dcp, executionYear);
+            final RootCurriculumGroup root = registration.getLastStudentCurricularPlan().getRoot();
+
+            final CourseGroup courseGroupA = new CourseGroup(dcp.getRoot(), "A", "A", executionInterval, null);
+            final CourseGroup courseGroupB = new CourseGroup(courseGroupA, "B", "B", executionInterval, null);
+            final CourseGroup courseGroupC = new CourseGroup(courseGroupB, "C", "C", executionInterval, null);
+
+            groupA = new CurriculumGroup(root, courseGroupA);
+            groupB = new CurriculumGroup(groupA, courseGroupB);
+            groupC = new CurriculumGroup(groupB, courseGroupC);
+
+            return null;
+        });
+    }
+
+    @Test
+    public void testCurriculumModule_ComparatorByNameAndId() {
+        List<CurriculumModule> modules = new ArrayList<>(List.of(groupC, groupA, groupB));
+        modules.sort(CurriculumModule.COMPARATOR_BY_NAME_AND_ID);
+
+        // sorted by group name: A < B < C
+        assertEquals(groupA, modules.get(0));
+        assertEquals(groupB, modules.get(1));
+        assertEquals(groupC, modules.get(2));
+
+        assertEquals(0, CurriculumModule.COMPARATOR_BY_NAME_AND_ID.compare(groupA, groupA));
+    }
+
+    @Test
+    public void testCurriculumModule_ComparatorByFullPathNameAndId() {
+        List<CurriculumModule> modules = new ArrayList<>(List.of(groupC, groupA, groupB));
+        modules.sort(CurriculumModule.COMPARATOR_BY_FULL_PATH_NAME_AND_ID);
+
+        // shared path prefix, ordered by the appended name segment: A < B < C
+        assertEquals(groupA, modules.get(0));
+        assertEquals(groupB, modules.get(1));
+        assertEquals(groupC, modules.get(2));
+
+        assertTrue(groupA.getFullPath().contains(" > A"));
+        assertEquals(0, CurriculumModule.COMPARATOR_BY_FULL_PATH_NAME_AND_ID.compare(groupA, groupA));
+    }
+
+    @Test
+    public void testCurriculumModule_ComparatorByCreationDate() {
+        groupA.setCreationDateDateTime(new DateTime(2020, 1, 1, 0, 0, 0));
+        groupB.setCreationDateDateTime(new DateTime(2021, 6, 15, 12, 0, 0));
+        groupC.setCreationDateDateTime(new DateTime(2022, 12, 31, 23, 59, 59));
+
+        List<CurriculumModule> modules = new ArrayList<>(List.of(groupC, groupA, groupB));
+        modules.sort(CurriculumModule.COMPARATOR_BY_CREATION_DATE);
+
+        assertEquals(groupA, modules.get(0));
+        assertEquals(groupB, modules.get(1));
+        assertEquals(groupC, modules.get(2));
+
+        assertEquals(0, CurriculumModule.COMPARATOR_BY_CREATION_DATE.compare(groupA, groupA));
+    }
+
+    private static DegreeCurricularPlan createDcp(final ExecutionYear executionYear) {
+        final DegreeType degreeType = new DegreeType(new LocalizedString.Builder().with(Locale.getDefault(), "Degree").build());
+        degreeType.setCode("D" + UUID.randomUUID());
+
+        final Degree degree = DegreeTest.createDegree(degreeType, "D" + UUID.randomUUID(), "Curriculum Module Test Degree",
+                executionYear);
+        final DegreeCurricularPlan dcp = degree.createDegreeCurricularPlan("Curriculum Module Test Degree Plan",
+                User.findByUsername(UserUtil.ADMIN_USERNAME).getPerson(), AcademicPeriod.THREE_YEAR);
+        dcp.createExecutionDegree(executionYear);
+        return dcp;
+    }
+}
